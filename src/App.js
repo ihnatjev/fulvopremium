89297ada7a368bba40bb2b36b1b2e1bc7daa6748
Serhij Ihnatjev <91773782+ihnatjev@users.noreply.github.com>
2022-06-09 18:07:07 +0300

    add routes and dependencies

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,19 +8,61 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload with GitHub Actions.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
       </header>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about" className="App-link">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/" className="App-link">Home</Link>
+      </nav>
+    </>
+  );
+}
+
+function NotFound() {
+  return (
+    <>
+      <main>
+        <h2>Content Not Found</h2>
+        <p>
+          Ooooops...
+        </p>
+      </main>
+      <nav>
+        <Link to="/" className="App-link">Home</Link>
+      </nav>
+    </>
   );
 }
 
