dce3c5a78de1b6715460ef00a0b420f2a6d1088e
Serhij Ihnatjev <91773782+ihnatjev@users.noreply.github.com>
2022-06-09 15:44:12 +0000

    Updates

--- precache-manifest.076bf4a303771d1e192e847bceb7a9bb.js
+++ precache-manifest.076bf4a303771d1e192e847bceb7a9bb.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "2f4b973ed6a1f383aa1d",
+    "revision": "4d29bc93d8f636ebb07e",
     "url": "/static/css/main.a00801a3.chunk.css"
   },
   {
-    "revision": "2f4b973ed6a1f383aa1d",
-    "url": "/static/js/main.2f4b973e.chunk.js"
+    "revision": "4d29bc93d8f636ebb07e",
+    "url": "/static/js/main.4d29bc93.chunk.js"
   },
   {
     "revision": "7d8a5b69bced8be18895",
     "url": "/static/js/1.7d8a5b69.chunk.js"
   },
   {
-    "revision": "e6b3b0331ca83a6e31c3",
-    "url": "/static/js/2.e6b3b033.chunk.js"
+    "revision": "6eef03d83a5acdd41106",
+    "url": "/static/js/2.6eef03d8.chunk.js"
   },
   {
     "revision": "43928d61158871188870",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/static/media/logo.06e73328.svg"
   },
   {
-    "revision": "ae417016118f51bd0410810dd6dc03f3",
+    "revision": "1f6a28c685ec8fac4c74c1ee8f4cfb81",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.416f6bebbb319f2f571ae5907ddb2ef3.js"
+  "/precache-manifest.076bf4a303771d1e192e847bceb7a9bb.js"
 );
 
 workbox.clientsClaim();
