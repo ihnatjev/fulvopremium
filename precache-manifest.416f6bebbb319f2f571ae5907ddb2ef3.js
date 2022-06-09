2fbea81eb477f863213569d21f66e3932c6a0b66
Serhij Ihnatjev <91773782+ihnatjev@users.noreply.github.com>
2022-06-09 15:12:46 +0000

    Updates

---
+++ precache-manifest.416f6bebbb319f2f571ae5907ddb2ef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f4b973ed6a1f383aa1d",
+    "url": "/static/css/main.a00801a3.chunk.css"
+  },
+  {
+    "revision": "2f4b973ed6a1f383aa1d",
+    "url": "/static/js/main.2f4b973e.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "e6b3b0331ca83a6e31c3",
+    "url": "/static/js/2.e6b3b033.chunk.js"
+  },
+  {
+    "revision": "43928d61158871188870",
+    "url": "/static/js/runtime~main.43928d61.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ae417016118f51bd0410810dd6dc03f3",
+    "url": "/index.html"
+  }
+];
