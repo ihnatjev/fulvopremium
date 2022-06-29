cff9461f23752aa9bb8628bb7426a02ce92b4d01
Serhij Ihnatjev <91773782+ihnatjev@users.noreply.github.com>
2022-06-29 12:42:28 +0300

    add sticky footer

--- src/GlobalLayout.tsx
+++ src/GlobalLayout.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+const GlobalLayoutRoot = styled.div`
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+`;
+
+const Main = styled.main``;
+
 const GlobalLayout = () => (
-  <>
+  <GlobalLayoutRoot>
     <Header />
 
-    <main>
+    <Main>
       <Outlet />
-    </main>
+    </Main>
 
     <Footer />
-  </>
+  </GlobalLayoutRoot>
 );
 
 export default GlobalLayout;
